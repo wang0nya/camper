2d8d45c94a51f104bd8bef8153bf9c513a9332ac
wang0nya <kwangonya@gmail.com>
2018-05-13 22:39:04 +0300

    reset password

--- src/app/reset-password/reset-password.component.ts
+++ src/app/reset-password/reset-password.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AngularFireAuth} from 'angularfire2/auth';
 
 @Component({
   selector: 'app-reset-password',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reset-password.component.css']
 })
 export class ResetPasswordComponent implements OnInit {
-
-  constructor() { }
+success: any;
+error: any;
+  email = '';
+  constructor(private afAuth: AngularFireAuth) { }
 
   ngOnInit() {
   }
-
+  onSubmit(formData) {
+    this.sendPassword(
+      formData.value.email
+    );
+  }
+  sendPassword(email: string) {
+    this.afAuth.auth.sendPasswordResetEmail(email)
+      .then(() => {
+        console.log('email sent');
+        this.success = 'Check your email for a link to reset your password';
+      })
+      .catch(err => {
+        console.log('Something went wrong: ', err.message);
+        this.error = err.message;
+      });
+  }
 }
