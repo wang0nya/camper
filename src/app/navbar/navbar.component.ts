b2859952872c3cb45185f69450218eb0a6553f23
wang0nya <kwangonya@gmail.com>
2018-05-14 13:20:51 +0300

    navbar issues

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../../services/auth.service';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-navbar',
@@ -6,8 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
+  public user: any;
+  constructor(private authService: AuthService) { }
   ngOnInit() {
+    this.checkUser();
+    this.loadScript();
   }
-
+  checkUser() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.user = user;
+        console.log('logged in', user);
+      } else {
+        console.log('not logged in');
+      }
+    });
+  }
+  public loadScript() {
+    const body = <HTMLDivElement> document.body;
+    const script = document.createElement('script');
+    script.innerHTML = '';
+    script.src = '../../assets/js/paper-kit.js';
+    script.async = true;
+    script.defer = true;
+    body.appendChild(script);
+  }
 }
