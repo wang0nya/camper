6ca1fb289d00a7cfd86ab5e2279cc04e42122781
wang0nya <kwangonya@gmail.com>
2018-05-18 00:53:41 +0300

    fix Property 'firebase' does not exist on type '{ production: boolean; }'

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDUWTTaq1Eo9fRGs4buRutSEktNZUy8JtI',
+    authDomain: 'camper-398a1.firebaseapp.com',
+    databaseURL: 'https://camper-398a1.firebaseio.com',
+    projectId: 'camper-398a1',
+    storageBucket: 'camper-398a1.appspot.com',
+    messagingSenderId: '473864816401'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebase: {
     apiKey: 'AIzaSyDUWTTaq1Eo9fRGs4buRutSEktNZUy8JtI',
     authDomain: 'camper-398a1.firebaseapp.com',
